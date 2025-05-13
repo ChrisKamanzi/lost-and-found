18003fbcd82e450bdbd68c03924415b7e6d339be
user <chrisibambe@gmail.com>
2025-05-13 13:49:16 +0200

    Added unit card_detail unit testing

---
+++ test/mockDio.dart
@@ -0,0 +1,6 @@
+import 'package:mockito/annotations.dart';
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+@GenerateMocks([Dio, SharedPreferences])
+void main() {}
