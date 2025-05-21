cfb6c8bb5d5fa689902be3c8a9bc80426df4eafd
user <chrisibambe@gmail.com>
2025-05-21 16:22:07 +0200

    separated business  logic with the UI : pages

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class MyApp extends ConsumerWidget {
       GoRoute(
         path: '/signUp',
         builder: (BuildContext context, GoRouterState state) {
-          return sign_up();
+          return SignUp();
         },
       ),
       GoRoute(
---
+++ lib/pages/services/pages.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+final pages = [
+  {
+    'title': 'Report Found',
+    'description':
+    'An item lost and found can easily be reported or an ad could be created',
+    'icon': Icons.report,
+  },
+  {
+    'title': 'Search Map',
+    'description':
+    'Search for found items or found close to your location be showed on the map',
+    'icon': Icons.map,
+  },
+  {
+    'title': 'Messaging',
+    'description': 'last app allows interaction between users',
+    'icon': Icons.message,
+  },
+];
