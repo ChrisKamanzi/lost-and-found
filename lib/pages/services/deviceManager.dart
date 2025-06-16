e8f80260d19babda2005ab73fb9681cedb75cd5e
user <chrisibambe@gmail.com>
2025-06-16 14:06:36 +0200

    added device unfo plus package to save the device ID

---
+++ lib/pages/services/deviceManager.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+
+class Devicemanager {
+  static Future<String?> getDeviceId() async {
+    DeviceInfoPlugin deviceinfo = DeviceInfoPlugin();
+
+    try {
+      if (Platform.isAndroid) {
+        AndroidDeviceInfo android = await deviceinfo.androidInfo;
+        return android.id;
+      } else if (Platform.isIOS) {
+        IosDeviceInfo iosInfo = await deviceinfo.iosInfo;
+        return iosInfo.identifierForVendor;
+      } else {
+        return "Unsupported platform";
+      }
+    } catch (e) {
+      print('failed to frtch the ID : $e');
+      return null;
+    }
+  }
+}
