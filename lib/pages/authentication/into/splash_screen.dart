9e540af6f3f88b6c82e8a3176295fcd51d9dee67
user <chrisibambe@gmail.com>
2025-06-23 15:34:33 +0200

    security: applied the certificate package and initiated  it on the splash screen

--- lib/pages/authentication/into/splash_screen.dart
+++ lib/pages/authentication/into/splash_screen.dart
@@ -1,14 +1,23 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../../../stateManagment/Notifier/splash_notifier.dart';
+import '../../services/caertificate_pinning.dart';
 
 class SplashScreen extends ConsumerWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // One-time trigger to run cert check when the widget is first built
+    Future.microtask(() async {
+      if (Platform.isAndroid || Platform.isIOS) {
+        await CertificatePinningService.checkServerCertificate();
+      }
+    });
 
+    // Navigation logic
     ref.listen<String?>(splashProvider, (prev, next) {
       if (next != null) {
         context.go(next);
