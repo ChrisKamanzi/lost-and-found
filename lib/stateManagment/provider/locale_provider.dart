89eef2c68d16c591acd7a7b1c1ff52e1d1b12d6a
user <chrisibambe@gmail.com>
2025-07-22 11:45:57 +0200

    worked on the fake notifier and locale notifier

--- lib/stateManagment/provider/locale_provider.dart
+++ lib/stateManagment/provider/locale_provider.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../Notifier/localeNotifier.dart';
+import '../Notifier/locale_notifier.dart';
 
 final localeProvider = StateNotifierProvider<LocaleNotifier, Locale>((ref) {
   return LocaleNotifier();
